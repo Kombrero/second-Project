a3b725cb48387d4e7d7db6de3c97d61b9cb343cc
Slava Kovalev <reqwiem96@mail.ru>
2020-04-28 10:40:41 +0300

    23 дз

--- scripts/script.js
+++ scripts/script.js
@@ -277,7 +277,6 @@ window.addEventListener('DOMContentLoaded', () => {
         elem.addEventListener('mouseover', event => {
             firstImg = event.target.src;
             event.target.src = event.target.dataset.img;
-            //event.target.dataset.img = event.target.src.replace(/[a].jpg/g, '.jpg');
         });
 
         elem.addEventListener('mouseout', event => {
@@ -286,7 +285,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     console.log(comandPerson);
 
-    //Калькулятор
+    //Калькулятор ввод
 
     const calcBlock = document.querySelector('.calc-block'),
         calcInput = calcBlock.querySelectorAll('input');
@@ -301,6 +300,52 @@ window.addEventListener('DOMContentLoaded', () => {
             console.log(elem.value);
         });
 
-        console.log(elem);
     });
+
+    // Калькуятор расчет
+
+    const calc = (price = 100) => {
+        const calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = +calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if(calcDay && calcDay.value < 5 ){
+                dayValue *= 2;
+            }else if (calcDay && calcDay.value < 10){
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+
+            totalValue.textContent = total;
+        };
+
+
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+            if (target === calcType || target === calcSquare || target === calcDay || target === calcCount) {
+                countSum();
+            }
+
+        });
+
+    };
+
+    calc(100);
 });
